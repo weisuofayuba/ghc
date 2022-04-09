725e5c2e1bd301a2c108c2a7d9014a04969eeb41
Carrie Xu <carrie.xmy@gmail.com>
2022-04-09 16:49:06 +0800

    Change test should_fail/records-poly-update to should_run

--- testsuite/tests/patsyn/should_run/records-poly-update.hs
+++ testsuite/tests/patsyn/should_run/records-poly-update.hs
@@ -8,6 +8,10 @@ data A = A deriving Show
 
 p1 = Just True
 
+-- Record updates is desugared to case belowexpression, see #18802.
+-- p6 = case p1 of ReqNoProv x -> ReqNoProv A
+-- (Detials can be found in Note [Record Updates] defined in Tc.Gen.Expr.hs)
+
 p6 = p1 {j = A}
 
 main = print p6
